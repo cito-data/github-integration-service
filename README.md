e19196572f81d346fafe6601056c8c570f908297
Felix Brockmeier <felix.brockmeier@outlook.com>
2022-06-23 16:15:56 +0200

    minor fixes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # github-integration-service
+
+smee --url https://smee.io/SkmpvHU6eEirh4Np --path /api/github/webhooks --port 3001
--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,18 @@
-const privateKey = process.env.GITHUB_PRIVATE_KEY || '';
-const appId = process.env.GITHUB_APP_IDENTIFIER || '';
-const webhookSecret = process.env.GITHUB_WEBHOOK_SECRET || '';
-const clientId = process.env.GITHUB_APP_CLIENT_ID || '';
-const clientSecret = process.env.GITHUB_APP_CLIENT_SECRET || '';
+const privateKey = process.env.GITHUB_PRIVATE_KEY;
+if(!privateKey) throw new Error('Private key not available');
+
+const appId = process.env.GITHUB_APP_IDENTIFIER ? (process.env.GITHUB_APP_IDENTIFIER, 10): '';
+if(!appId) throw new Error('App id not available');
+
+const webhookSecret = process.env.GITHUB_WEBHOOK_SECRET;
+if(!webhookSecret) throw new Error('Webhook secret not available');
+
+const clientId = process.env.GITHUB_APP_CLIENT_ID;
+if(!clientId) throw new Error('Client id not available');
+
+const clientSecret = process.env.GITHUB_APP_CLIENT_SECRET;
+if(!clientSecret) throw new Error('Client secret not available');
+
 export const nodeEnv = process.env.NODE_ENV || 'development';
 export const defaultPort = 3002;
 export const port = process.env.PORT
