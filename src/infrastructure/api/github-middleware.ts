06733ff1b45e57691cb40405e882e23e9dcb022b
Felix Brockmeier <felix.brockmeier@outlook.com>
2022-09-03 16:54:47 +0200

    added githug middleware dev env

--- src/infrastructure/api/github-middleware.ts
+++ src/infrastructure/api/github-middleware.ts
@@ -1,6 +1,7 @@
-import { Endpoints } from '@octokit/types';
+// import { Endpoints } from '@octokit/types';
 import axios, { AxiosRequestConfig } from 'axios';
 import { App } from 'octokit';
+import EventSource from 'eventsource';
 import { GithubProfile } from '../../domain/entities/github-profile';
 import { appConfig } from '../../config';
 import getRoot from '../shared/api-root-builder';
@@ -181,147 +182,147 @@ export default (config: GithubConfig): App => {
     }
   };
 
-  const requestLineageCreation = async (
-    catalogText: string,
-    manifestText: string,
-    organizationId: string
-  ): Promise<any> => {
-    try {
-      const jwt = await getJwt();
+  // const requestLineageCreation = async (
+  //   catalogText: string,
+  //   manifestText: string,
+  //   organizationId: string
+  // ): Promise<any> => {
+  //   try {
+  //     const jwt = await getJwt();
 
-      const configuration: AxiosRequestConfig = {
-        headers: {
-          Authorization: `Bearer ${jwt}`,
-          'Content-Type': 'application/json',
-        },
-      };
+  //     const configuration: AxiosRequestConfig = {
+  //       headers: {
+  //         Authorization: `Bearer ${jwt}`,
+  //         'Content-Type': 'application/json',
+  //       },
+  //     };
 
-      const gateway =
-        appConfig.express.mode === 'production'
-          ? 'kga7x5r9la.execute-api.eu-central-1.amazonaws.com/production'
-          : 'localhost:3000';
+  //     const gateway =
+  //       appConfig.express.mode === 'production'
+  //         ? 'kga7x5r9la.execute-api.eu-central-1.amazonaws.com/production'
+  //         : 'localhost:3000';
 
-      const apiRoot = await getRoot(gateway, 'api/v1');
+  //     const apiRoot = await getRoot(gateway, 'api/v1');
 
-      const response = await axios.post(
-        `${apiRoot}/lineage`,
-        {
-          catalog: catalogText,
-          manifest: manifestText,
-          targetOrganizationId: organizationId,
-        },
-        configuration
-      );
+  //     const response = await axios.post(
+  //       `${apiRoot}/lineage`,
+  //       {
+  //         catalog: catalogText,
+  //         manifest: manifestText,
+  //         targetOrganizationId: organizationId,
+  //       },
+  //       configuration
+  //     );
 
-      const jsonResponse = response.data;
-      console.log(jsonResponse);
-      if (response.status === 200) return jsonResponse;
-      throw new Error(jsonResponse.message);
-    } catch (error: unknown) {
-      if (typeof error === 'string') return Promise.reject(error);
-      if (error instanceof Error) return Promise.reject(error.message);
-      return Promise.reject(new Error('Unknown error occured'));
-    }
-  };
+  //     const jsonResponse = response.data;
+  //     console.log(jsonResponse);
+  //     if (response.status === 200) return jsonResponse;
+  //     throw new Error(jsonResponse.message);
+  //   } catch (error: unknown) {
+  //     if (typeof error === 'string') return Promise.reject(error);
+  //     if (error instanceof Error) return Promise.reject(error.message);
+  //     return Promise.reject(new Error('Unknown error occured'));
+  //   }
+  // };
 
-  const handleInstallationCreated = async (payload: any): Promise<void> => {
-    const currentInstallation = payload.installation?.id;
-    if (!currentInstallation) throw Error('Current installation not found');
+  // const handleInstallationCreated = async (payload: any): Promise<void> => {
+  //   const currentInstallation = payload.installation?.id;
+  //   if (!currentInstallation) throw Error('Current installation not found');
 
-    const githubProfile = await getGithubProfile(
-      new URLSearchParams({ installationId: currentInstallation.toString(10) })
-    );
+  //   const githubProfile = await getGithubProfile(
+  //     new URLSearchParams({ installationId: currentInstallation.toString(10) })
+  //   );
 
-    const { organizationId, firstLineageCreated } = githubProfile;
+  //   const { organizationId, firstLineageCreated } = githubProfile;
 
-    if (firstLineageCreated) return;
+  //   if (firstLineageCreated) return;
 
-    const catalogRes = await githubApp.octokit.request('GET /search/code', {
-      q: `filename:catalog+extension:json+repo:${payload.repository.owner.login}/${payload.repository.name}`,
-    });
+  //   const catalogRes = await githubApp.octokit.request('GET /search/code', {
+  //     q: `filename:catalog+extension:json+repo:${payload.repository.owner.login}/${payload.repository.name}`,
+  //   });
 
-    let { data }: any = catalogRes;
-    let { items } = data;
+  //   let { data }: any = catalogRes;
+  //   let { items } = data;
 
-    if (items.length > 1) throw Error('More than 1 catalog found');
+  //   if (items.length > 1) throw Error('More than 1 catalog found');
 
-    let [item] = items;
-    let { path } = item;
+  //   let [item] = items;
+  //   let { path } = item;
 
-    const endpoint = 'GET /repos/{owner}/{repo}/contents/{path}';
+  //   const endpoint = 'GET /repos/{owner}/{repo}/contents/{path}';
 
-    type ContentResponseType = Endpoints[typeof endpoint]['response'];
-    const catalogResponse: ContentResponseType =
-      await githubApp.octokit.request(endpoint, {
-        owner: payload.repository.owner.login,
-        repo: payload.repository.name,
-        path,
-      });
+  //   type ContentResponseType = Endpoints[typeof endpoint]['response'];
+  //   const catalogResponse: ContentResponseType =
+  //     await githubApp.octokit.request(endpoint, {
+  //       owner: payload.repository.owner.login,
+  //       repo: payload.repository.name,
+  //       path,
+  //     });
 
-    if (catalogResponse.status !== 200)
-      throw new Error('Reading catalog failed');
+  //   if (catalogResponse.status !== 200)
+  //     throw new Error('Reading catalog failed');
 
-    // todo - include type checking
-    ({ data } = catalogResponse);
-    let { content, encoding } = data;
+  //   // todo - include type checking
+  //   ({ data } = catalogResponse);
+  //   let { content, encoding } = data;
 
-    if (typeof content !== 'string')
-      throw new Error(
-        'Did not receive content field in string format from API'
-      );
-    if (encoding !== 'base64') throw new Error('Unexpected encoding type');
+  //   if (typeof content !== 'string')
+  //     throw new Error(
+  //       'Did not receive content field in string format from API'
+  //     );
+  //   if (encoding !== 'base64') throw new Error('Unexpected encoding type');
 
-    const catalogBuffer = Buffer.from(content, encoding);
-    const catalogText = catalogBuffer.toString('utf-8');
+  //   const catalogBuffer = Buffer.from(content, encoding);
+  //   const catalogText = catalogBuffer.toString('utf-8');
 
-    const manifestRes = await githubApp.octokit.request('GET /search/code', {
-      q: `filename:manifest+extension:json+repo:${payload.repository.owner.login}/${payload.repository.name}`,
-    });
+  //   const manifestRes = await githubApp.octokit.request('GET /search/code', {
+  //     q: `filename:manifest+extension:json+repo:${payload.repository.owner.login}/${payload.repository.name}`,
+  //   });
 
-    ({ data } = manifestRes);
-    ({ items } = data);
+  //   ({ data } = manifestRes);
+  //   ({ items } = data);
 
-    if (items.length > 1) throw Error('More than 1 manifest found');
+  //   if (items.length > 1) throw Error('More than 1 manifest found');
 
-    [item] = items;
-    ({ path } = item);
+  //   [item] = items;
+  //   ({ path } = item);
 
-    const manifestResponse: ContentResponseType =
-      await githubApp.octokit.request(endpoint, {
-        owner: payload.repository.owner.login,
-        repo: payload.repository.name,
-        path,
-      });
+  //   const manifestResponse: ContentResponseType =
+  //     await githubApp.octokit.request(endpoint, {
+  //       owner: payload.repository.owner.login,
+  //       repo: payload.repository.name,
+  //       path,
+  //     });
 
-    if (manifestResponse.status !== 200)
-      throw new Error('Reading manifest failed');
+  //   if (manifestResponse.status !== 200)
+  //     throw new Error('Reading manifest failed');
 
-    // todo - include type checking
-    ({ data } = manifestResponse);
-    ({ content, encoding } = data);
+  //   // todo - include type checking
+  //   ({ data } = manifestResponse);
+  //   ({ content, encoding } = data);
 
-    if (typeof content !== 'string')
-      throw new Error(
-        'Did not receive content field in string format from API'
-      );
-    if (encoding !== 'base64') throw new Error('Unexpected encoding type');
+  //   if (typeof content !== 'string')
+  //     throw new Error(
+  //       'Did not receive content field in string format from API'
+  //     );
+  //   if (encoding !== 'base64') throw new Error('Unexpected encoding type');
 
-    const manifestBuffer = Buffer.from(content, encoding);
-    const manifestText = manifestBuffer.toString('utf-8');
+  //   const manifestBuffer = Buffer.from(content, encoding);
+  //   const manifestText = manifestBuffer.toString('utf-8');
 
-    const result = await requestLineageCreation(
-      catalogText,
-      manifestText,
-      organizationId
-    );
+  //   const result = await requestLineageCreation(
+  //     catalogText,
+  //     manifestText,
+  //     organizationId
+  //   );
 
-    if (result)
-      await updateGithubProfile(
-        currentInstallation.toString(10),
-        organizationId,
-        true
-      );
-  };
+  //   if (result)
+  //     await updateGithubProfile(
+  //       currentInstallation.toString(10),
+  //       organizationId,
+  //       true
+  //     );
+  // };
 
   const handleInstallationDeleted = async (payload: any): Promise<void> => {
     const currentInstallation = payload.installation.id;
@@ -379,11 +380,10 @@ export default (config: GithubConfig): App => {
     );
   };
 
-  githubApp.webhooks.onAny(async ({ id, name, payload }) => {
-    const body: any = payload;
+  const handleEvent = async (id: string, name: string, payload: any): Promise<void> => {
     console.log(`Receiving GitHub event - id: ${id}, name: ${name}`);
 
-    const event = body.action ? `${name}.${body.action}` : name;
+    const event = payload.action ? `${name}.${payload.action}` : name;
 
     const handlingMessage = `Handling ${event} GitHub event`;
 
@@ -391,7 +391,7 @@ export default (config: GithubConfig): App => {
       switch (event) {
         case 'installation.created':
           console.log(handlingMessage);
-          handleInstallationCreated(payload);
+          // handleInstallationCreated(payload);
           break;
         case 'installation.deleted':
           console.log(handlingMessage);
@@ -420,7 +420,19 @@ export default (config: GithubConfig): App => {
         console.error(`Error in github-webhook-middleware: ${error.message}`);
       console.error('Unknown error occured in github-webhook-middleware');
     }
-  });
+  };
+
+  if (appConfig.express.mode === 'development') {
+    const webhookProxyUrl = 'https://smee.io/jn9bVXprxGxZMZD';
+    const source = new EventSource(webhookProxyUrl);
+    source.onmessage = (event: any) => {
+      const webhookEvent = JSON.parse(event.data);
+      handleEvent(webhookEvent['x-github-delivery'], webhookEvent['x-github-event'], webhookEvent.body);
+    };
+  } else
+    githubApp.webhooks.onAny(async ({ id, name, payload }) =>
+      handleEvent(id, name, payload)
+    );
 
   return githubApp;
 };
